bcf6802deefe73ca878b8ad72a13a840a0c94221
yuxp <1039354317@qq.com>
2026-07-29 15:30:02 +0800

    feat: add something

--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -74,6 +74,12 @@ var config_default = defineConfig({
   clientId: process.env.TINA_CLIENT_ID || "",
   token: process.env.TINA_TOKEN || "",
   build: { outputFolder: "admin", publicFolder: "public" },
+  search: {
+    tina: {
+      indexerToken: process.env.TINA_SEARCH_TOKEN || "",
+      stopwordLanguages: ["eng"]
+    }
+  },
   media: { loadCustomStore: async () => (await Promise.resolve().then(() => (init_r2_media_store(), r2_media_store_exports))).R2MediaStore },
   schema: { collections: [
     { name: "products", label: "Products", path: "src/content/products", format: "md", fields: [text("name"), text("category"), text("status"), { type: "datetime", name: "launchDate", label: "Launch date" }, { type: "datetime", name: "updatedDate", label: "Updated date" }, { type: "boolean", name: "featured", label: "Featured" }, { type: "image", name: "image", label: "Main image" }, { type: "string", name: "description", label: "Description", ui: { component: "textarea" } }, { type: "object", name: "specs", label: "Specifications", list: true, fields: [text("label"), text("value")] }, { type: "string", name: "customization", label: "Customization", ui: { component: "textarea" } }, { type: "rich-text", name: "body", label: "Body", isBody: true }] },
